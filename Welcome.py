5c5d043332a609389e67a90072cd79a9b6aae8d6
Joseph Diaz <joseph.a.g.diaz@gmail.com>
2022-10-02 17:26:02 -0700

    Redoing plots.

--- Welcome.py
+++ Welcome.py
@@ -240,15 +240,6 @@ with st.expander("\U0001F52C My Research"):
     """)
 
     st.video(open("lorenz63.mp4", 'rb').read())
-    # file_ = open("lorenz63.gif", "rb")
-    # contents = file_.read()
-    # data_url = base64.b64encode(contents).decode("utf-8")
-    # file_.close()
-
-    # st.markdown(
-        # f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-        # unsafe_allow_html=True,
-    # )
 
     st.markdown(    
     f"""
